a3e0d3dd56e0ab1112e2db0e9f5bf57a06cd2a23
Liu-database <110245553+Liu-database@users.noreply.github.com>
2022-07-31 18:15:07 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master test!");
         System.out.println("hot-fix test!");
         System.out.println("git@github.com:Liu-database/git-test.git");
+        System.out.println("new1 test");
     }
 }
